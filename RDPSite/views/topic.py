4ec16377e22a36a97feb06a701758cd4bf25e287
dapeng0802 <08281048@bjtu.edu.cn>
2015-10-21 23:13:27 +0800

    add 'topic/profile.html' etc.

--- RDPSite/views/topic.py
+++ RDPSite/views/topic.py
@@ -33,3 +33,32 @@ def get_index(request):
     active_page = 'topic'
     # locals()返回一个包含当前作用域里面的所有变量和它们的值的字典。
     return render_to_response('topic/topics.html', locals(), context_instance=RequestContext(request))
+
+def get_profile(request, uid):
+    try:
+        if uid.isdigit():
+            user_info = SiteUser.objects.get(pk=uid)
+        else:
+            user_info = SiteUser.objects.get(username=uid)
+    except SiteUser.DoesNotExist:
+        raise Http404
+    
+    try:
+        current_page = int(request.GET.get('p', '1'))
+    except ValueError:
+        current_page = 1
+        
+    counter = {
+            'topics': user_info.topic_author.all().count(),
+            'replies': user_info.reply_author.all().count(),
+            'favorites': user_info.fav_user.all().count()
+    }
+    
+    user = request.user
+    if user.is_authenticated():
+        notifications_count = user.notify_user.filter(status=0).count()
+        
+    topics, topic_page = Topic.objects.get_user_all_topics(user_info.id, current_page=current_page)
+    replies, reply_page = Reply.objects.get_user_all_replies(user_info.id, current_page=current_page)
+    active_page = '_blank'
+    return render_to_response('topic/profile.html', locals(), context_instance=RequestContext(request))
